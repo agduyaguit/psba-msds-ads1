f0ec08939931bbbf516fe096aeb3bc0ac5147aff
Arnil Duyaguit <neil.gd@gmail.com>
2025-09-25 19:22:51 +0800

    Solve exercise 1

--- Unit02/reading_files.py
+++ Unit02/reading_files.py
@@ -19,6 +19,7 @@ def plot_yearly_gdp(df_gdp):
 
 
 def plot_yearly_gdp_relative(df_gdp):
+
     plt.figure(dpi=144)
 
     countries = df_gdp.columns
@@ -26,12 +27,17 @@ def plot_yearly_gdp_relative(df_gdp):
         if '_rel' not in country or 'United States' in country:
             continue
 
-        plt.plot(country)
+        plt.plot(df_gdp.index, df_gdp[country], label=country.strip('_rel'))
+
+    plt.legend()
+    plt.xlabel('Year')
+    plt.ylabel('Relative GDP (% US $)')
+    plt.show()
     return
 
 
 df_gdp = pd.read_csv('./Unit02/GDP_2015dollars.csv', index_col='Year')
-df_gdp.head()
+# df_gdp.head()
 # plot_yearly_gdp(df_gdp)
 
 for country in df_gdp.columns:
@@ -39,4 +45,5 @@ for country in df_gdp.columns:
                                 df_gdp['United States']) * 100
 
 df_gdp.head()
-print(df_gdp)
+#  print(df_gdp)
+plot_yearly_gdp_relative(df_gdp)
