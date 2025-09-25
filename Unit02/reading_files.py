0243d29d8e4672bd31a1a452a9d878e5b0d48dd7
Arnil Duyaguit <neil.gd@gmail.com>
2025-09-25 20:01:24 +0800

    Clean up code

--- Unit02/reading_files.py
+++ Unit02/reading_files.py
@@ -37,13 +37,10 @@ def plot_yearly_gdp_relative(df_gdp):
 
 
 df_gdp = pd.read_csv('./Unit02/GDP_2015dollars.csv', index_col='Year')
-# df_gdp.head()
 # plot_yearly_gdp(df_gdp)
 
 for country in df_gdp.columns:
     df_gdp[country + '_rel'] = (df_gdp[country] /
                                 df_gdp['United States']) * 100
 
-df_gdp.head()
-#  print(df_gdp)
 plot_yearly_gdp_relative(df_gdp)
